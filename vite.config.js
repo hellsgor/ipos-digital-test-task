9277bc908b1ea91f35f469c09a1b28e74728bf92
Igor Storchevoy <hellsgor@gmail.com>
2024-05-13 16:43:47 +0400

    feat(skeleton): добавлен handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import {defineConfig} from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { store } from './store.js';
+import handlebars from 'vite-plugin-handlebars';
+
+const pageData = {
+  '/index.html': store,
+};
 
 export default defineConfig({
   css: {
     devSourcemap: true,
+    plugins: [
+      handlebars({
+        partialDirectory: resolve(__dirname, './src/components'),
+        context(pagePath) {
+          return pageData[pagePath];
+        },
+      }),
+    ],
   },
-})
+});
