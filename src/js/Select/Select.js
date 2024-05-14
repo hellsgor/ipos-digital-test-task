152c9e82fcda2f9ad2f6fdd88475c2546417dbfc
Igor Storchevoy <hellsgor@gmail.com>
2024-05-14 11:36:17 +0400

    feat(Select): реализован метод getElement

--- src/js/Select/Select.js
+++ src/js/Select/Select.js
@@ -1,8 +1,27 @@
 class Select {
   select = null;
+  label = null;
+  button = null;
+  text = null;
+  dropdown = null;
+
+  classNames = {
+    label: `${selectClassName}__label`,
+    button: `${selectClassName}__button`,
+    text: `${selectClassName}__text`,
+    dropdown: `${selectClassName}__dropdown`,
+  };
 
   constructor(select) {
-    this.select = select || null;
+    this.select = select;
+
+    this.getElements();
+  }
+
+  getElements() {
+    Object.keys(this.classNames).forEach((key) => {
+      this[key] = this.select.querySelector(`.${this.classNames[key]}`);
+    });
   }
 }
 
